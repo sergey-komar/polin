2e701cbf001b1bed3e3d06ec6f07c8bc2b9bad53
sergey-komar <kursev81@yandex.ru>
2023-08-02 23:19:46 +0300

    firts commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
 
 })
+window.addEventListener('DOMContentLoaded', () => {
+
+const sityModal = document.querySelector('.sity-modal'),
+    sityBtn = document.querySelector('.header__top-sity');
+
+sityBtn.addEventListener('click', () => {
+    sityModal.classList.toggle('show');
+   
+})
+
+
+
+});
