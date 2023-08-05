06c99b6e08376353bfe6b7aca0d5ddad3ec5dd6b
sergey-komar <kursev81@yandex.ru>
2023-08-05 15:40:07 +0300

    home

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,44 @@
 $(function () {
 
-})
+    $('.card-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+         appendArrows: '.card-top__arrows',
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+    })
+
+    $('.bestsellers-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+         appendArrows: '.bestsellers-top__arrows',
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev-bestsellers"></button>',
+        nextArrow: '<button type="button" class="slick-next-bestsellers"></button>'
+    });
+
+    $('.merch-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+         appendArrows: '.merch-top__arrows',
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev-merch"></button>',
+        nextArrow: '<button type="button" class="slick-next-merch"></button>'
+    });
+
+    $('.home-news__slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+         appendArrows: '.home-news__top-arrows',
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev-home-news"></button>',
+        nextArrow: '<button type="button" class="slick-next-home-news"></button>'
+    });
+});
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
 const sityModal = document.querySelector('.sity-modal'),
@@ -9,7 +47,7 @@ const sityModal = document.querySelector('.sity-modal'),
 sityBtn.addEventListener('click', () => {
     sityModal.classList.toggle('show');
    
-})
+});
 
 
 
